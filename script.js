6a04b5ba86e75d5ab951a2da75eaba5e4cd8d0bd
Varun Bhabhra <75928040+Varun-Bhabhra@users.noreply.github.com>
2021-12-30 21:18:58 +0530

    new design

---
+++ script.js
@@ -0,0 +1,7 @@
+﻿window.onload = () => {
+  document.querySelector('#loader-wrapper').style.display = "none";
+  document.querySelector('#loader').style.display = "none";
+}
+// window.onload = () => {
+//   document.querySelector('.ui').style.display = "none";
+// }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
       colors: {
         'dark': '#a1c4fd',
         'light': '#c2e9fb'
+      },
+      fontFamily: {
+        'josefin': 'Josefin Sans',
       }
     },
   },
