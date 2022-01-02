b6c0021c747655e828aabc7d6556928415ce6ea6
Varun Bhabhra <75928040+Varun-Bhabhra@users.noreply.github.com>
2022-01-02 18:41:44 +0530

    Panel Designed

--- script.js
+++ script.js
@@ -2,6 +2,3 @@
   document.querySelector('#loader-wrapper').style.display = "none";
   document.querySelector('#loader').style.display = "none";
 }
-// window.onload = () => {
-//   document.querySelector('.ui').style.display = "none";
-// }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,21 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'dark': '#a1c4fd',
-        'light': '#c2e9fb'
+        'dark': '#',
+        'light': '#'
       },
       fontFamily: {
         'josefin': 'Josefin Sans',
+      },
+      backgroundImage: {
+        'one': "url('/img/one.png')",
+        'onePot': "url('/img/onePot.png')",
+        'two': "url('/img/two.png')",
+        'twoPot': "url('/img/twoPot.png')",
+        'three': "url('/img/three.jpg')",
+        'threePot': "url('/img/threePot.jpg')",
+        'four': "url('/img/four.jpg')",
+        'fourPot': "url('/img/fourPot.jpg')",
       }
     },
   },
