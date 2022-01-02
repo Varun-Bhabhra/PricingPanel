aa73cba67e891fe6699993c6404fd104b9793db1
Varun Bhabhra <75928040+Varun-Bhabhra@users.noreply.github.com>
2022-01-02 21:52:20 +0530

    final

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-﻿window.onload = () => {
+﻿const downArrow = document.querySelector('#downArrow');
+const panels = document.querySelector('#panel_one');
+
+window.onload = () => {
   document.querySelector('#loader-wrapper').style.display = "none";
   document.querySelector('#loader').style.display = "none";
 }
+
+downArrow.addEventListener('click', () => {
+  panels.scrollIntoView({ behavior: 'smooth' })
+})
